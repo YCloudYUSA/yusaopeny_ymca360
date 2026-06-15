04af835eb19c8add2424ed06ed96791a5fe6a7c5
Avi Schwab <froboy@gmail.com>
2026-06-15 18:21:36 -0500

    Revert "Increase Guzzle request timeout from 60s to 120s"
    This reverts commit fa4750bf836c2b1cbd35cf499ac425b1e3d7e17e.

--- src/Y360Client.php
+++ src/Y360Client.php
@@ -203,7 +203,7 @@ class Y360Client {
     $options = array_merge([
       'headers' => ['Accept' => 'application/json'],
       'auth' => $this->getAuth(),
-      'timeout' => 120,
+      'timeout' => 60,
     ], $options);
 
     $queryString = $this->buildQueryString($params);
